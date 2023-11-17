d2688a244fbfd406cf0aa0459ea06372f7043242
carmobu <carlosmolanobustos@hotmail.com>
2023-11-17 08:05:27 -0500

    fase 1

---
+++ ejercicios.js
@@ -0,0 +1,25 @@
+/* 
+
+1R EJERCICIO:
+Super ejercicio: Preguntar nombre al usuario y saludarlo por su nombre.
+
+CODIGO:
+var nombreUsuario = prompt("Por favor, ingresa tu nombre:");
+document.write("Hola " + nombreUsuario + " Bienvenido.");
+
+<---------------------------------------------------------------------------------->
+
+2DO EJERCICIO:
+Preguntar nombre al usuario y saludarlo por su nombre.
+Si el usuario no escribe su nombre saludar: "Hola, desconocido."
+
+CODIGO:
+
+var nombreUsuario = prompt("ingresa tu nombre:");
+if (nombreUsuario == "") {
+    document.write("Hola desconocido, bienvenido.");
+} else {
+    document.write("Hola " + nombreUsuario + " bienvenido.");
+} 
+
+*/
